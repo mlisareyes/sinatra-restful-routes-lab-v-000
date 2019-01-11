db52a86c9a5b4deff6b7a98921f4438508b56179
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-01-11 04:41:36 +0000

    Automatically backed up by Learn

--- db/migrate/20190111044052_create_recipes.rb
+++ db/migrate/20190111044052_create_recipes.rb
@@ -1,4 +1,9 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
+    create table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.string :cook_time
+    end
   end
 end
